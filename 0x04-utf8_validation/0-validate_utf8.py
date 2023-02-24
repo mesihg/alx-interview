92da43ed87d337a5a7aad1027aef1cb1da5a2bb3
Mesih Grmay <mesihgrmay@yahoo.com>
2023-02-24 02:50:28 +0000

    Task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,25 +5,23 @@
 def validUTF8(data):
     """
     Determines if a given data set represents a valid UTF-8 encoding.
-
     :param data: list of integers representing 1 byte of data each
     :return: True if data is a valid UTF-8 encoding, else return False
     """
-    num_bytes_to_follow = 0
-    for byte in data:
-        if num_bytes_to_follow == 0:
-            if byte >> 7 == 0b0:
-                continue
-            elif byte >> 5 == 0b110:
-                num_bytes_to_follow = 1
-            elif byte >> 4 == 0b1110:
-                num_bytes_to_follow = 2
-            elif byte >> 3 == 0b11110:
-                num_bytes_to_follow = 3
-            else:
+    byte_count = 0
+
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
                 return False
         else:
-            if byte >> 6 != 0b10:
+            if i >> 6 != 0b10:
                 return False
-            num_bytes_to_follow -= 1
-    return num_bytes_to_follow == 0
+            byte_count -= 1
+    return byte_count == 0
