e24b04129ee91dafd7d2cf7166cb4abf03393fc9
Mesih Grmay <mesihgrmay@yahoo.com>
2023-03-16 13:08:17 +0000

    Rotate 2D Matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Rotate 2D Matrix module
+"""
+
+
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    # Reverse each row of the matrix
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
